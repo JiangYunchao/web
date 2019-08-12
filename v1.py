23d4989b9bb639d9b831b0cac650d8e63b257f64
jiang <504424843@qq.com>
2019-08-12 20:43:25 +0800

    update html code

--- v1.py
+++ v1.py
@@ -21,18 +21,26 @@ def index():
         else:
             return redirect(url_for('new_user'))
 
+
     return render_template('index.html')
 
 @app.route('/exist', methods=['GET', 'POST'])
 def exist_user():
     if request.method == "POST":
         if request.form['submit_button'] == "Confirm":
-            flash("Item Confirmed.")
-            return redirect(url_for('exist_user'))
-        else:
+            value = 0
+            s_option = request.values.getlist("img")
+            for s in s_option:
+                value += int(s)
+            return redirect(url_for('result', value=value))
+
+        elif request.form['submit_button'] == "New":
             flash("Change New Item.")
             return redirect(url_for('exist_user'))
+
+
     return render_template('exist.html')
+
 
 @app.route('/new', methods=['GET', 'POST'])
 def new_user():
@@ -40,10 +48,16 @@ def new_user():
         if request.form['submit_button'] == "Confirm":
             flash("Item Confirmed.")
             return redirect(url_for('new_user'))
+
         else:
             flash("Go to new page.")
             return redirect(url_for('new_user'))
+
+
     return render_template('new.html')
 
+@app.route('/result/<value>')
+def result(value):
+    return render_template('result.html', value=str(value))
 
     
